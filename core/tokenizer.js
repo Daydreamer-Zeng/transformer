2736a577b72a2c0b19c7944398d948051d22d1e4
Daydreamer <971915386@qq.com>
2026-05-15 10:41:37 +0800

    style: apply code formatting.

--- core/tokenizer.js
+++ core/tokenizer.js
@@ -30,7 +30,7 @@ export const TOKEN_TYPES = Object.freeze({
 
   // Others
   Whitespace: "Whitespace",
-  EOF: "EOF",
+  EOF: "EOF"
 });
 
 const EOF_TOKEN = {
@@ -49,17 +49,12 @@ const EOF_TOKEN = {
       column: 0,
       index: 0
     }
-  },
+  }
 };
 
 export default class Tokenizer {
   constructor() {
-    this.source = "";
-    this.tokens = [];
-    this.position = 0;
-    this.currentChar = null;
-    this.line = 1;
-    this.column = 0;
+    this.build("");
   }
 
   static KEYWORD = new Set([
@@ -102,6 +97,7 @@ export default class Tokenizer {
     "while",
     "with",
     "yield",
+
     // Strict Mode Keywords
     "implements",
     "interface",
@@ -110,8 +106,9 @@ export default class Tokenizer {
     "protected",
     "public",
     "static",
+
     // Future Reserved Keywords
-    "enum",
+    "enum"
   ]);
 
   static OPERATOR = new Set([
@@ -122,6 +119,7 @@ export default class Tokenizer {
     "/",
     "%",
     "**",
+
     // Assignment operators
     "=",
     "+=",
@@ -130,21 +128,25 @@ export default class Tokenizer {
     "/=",
     "%=",
     "**=",
+
     // Equality operators
     "==",
     "===",
     "!=",
     "!==",
+
     // Relational operators
     "<",
     ">",
     "<=",
     ">=",
+
     // Logical operators
     "!",
     "&&",
     "||",
     "??",
+
     // Bitwise operators
     "&",
     "|",
@@ -153,6 +155,7 @@ export default class Tokenizer {
     "<<",
     ">>",
     ">>>",
+
     // Bitwise assignment operators
     "&=",
     "|=",
@@ -160,20 +163,24 @@ export default class Tokenizer {
     "<<=",
     ">>=",
     ">>>=",
+
     // Increment/Decrement operators
     "++",
     "--",
+
     // Conditional operators
     "?",
     ":",
+
     // Other
     "=>",
-    "??=",
+    "??="
   ]);
 
   static PUNCTUATOR = new Set([
     // Spread/Rest operator
     "...",
+
     // Parentheses and braces
     "(",
     ")",
@@ -181,62 +188,42 @@ export default class Tokenizer {
     "}",
     "[",
     "]",
+
     // Separators
     ";",
     ",",
+
     // Accessors
     ".",
     "?.",
+
     // Other
     ":",
-    "?",
+    "?"
   ]);
 
   static NUMERIC_REGEX = {
     BINARY: /^0[bB][01]+$/,
     OCTAL: /^0[oO][0-7]+$/,
     DECIMAL: /^(\d+\.?\d*|\.\d+)([eE][+-]?\d+)?$/,
-    HEXADECIMAL: /^0[xX][0-9a-fA-F]+$/,
+    HEXADECIMAL: /^0[xX][0-9a-fA-F]+$/
   };
 
   static ASCII_WHITESPACE = new Set([" ", "\t", "\n", "\r", "\f", "\v"]);
 
-  static ASCII_IDENTIFIER_START = new Set(
-    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_$"
-  );
+  static ASCII_IDENTIFIER_START = new Set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_$");
 
-  static ASCII_IDENTIFIER = new Set(
-    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_$"
-  );
+  static ASCII_IDENTIFIER = new Set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_$");
 
-  static REGEX_START_KEYWORD = new Set([
-    "return",
-    "do",
-    "else",
-    "case",
-    "throw",
-  ]);
+  static REGEX_START_KEYWORD = new Set(["return", "do", "else", "case", "throw"]);
 
-  static REGEX_START_OPERATOR = new Set([
-    "(",
-    "[",
-    "{",
-    ",",
-    ";",
-    "=",
-    ":",
-    "=>",
-    "&&",
-    "||",
-    "?",
-    "...",
-  ]);
+  static REGEX_START_OPERATOR = new Set(["(", "[", "{", ",", ";", "=", ":", "=>", "&&", "||", "?", "..."]);
 
   build(source) {
     this.source = source;
     this.tokens = [];
     this.position = 0;
-    this.currentChar = this.source[0] || null;
+    this.currentChar = source[0] || null;
     this.line = 1;
     this.column = 0;
   }
@@ -250,10 +237,7 @@ export default class Tokenizer {
         continue;
       }
 
-      if (
-        this.currentChar === "/" &&
-        (this.peekChar() === "/" || this.peekChar() === "*")
-      ) {
+      if (this.currentChar === "/" && (this.peekChar() === "/" || this.peekChar() === "*")) {
         this.readComment();
         continue;
       }
@@ -278,22 +262,17 @@ export default class Tokenizer {
         continue;
       }
 
-      if (
-        Tokenizer.OPERATOR.has(this.currentChar) ||
-        Tokenizer.PUNCTUATOR.has(this.currentChar)
-      ) {
+      if (Tokenizer.OPERATOR.has(this.currentChar) || Tokenizer.PUNCTUATOR.has(this.currentChar)) {
         this.readOperatorOrPunctuator();
         continue;
       }
 
-      throw new Error(
-        `Unexpected token: ${this.currentChar} at line ${this.line}, column ${this.column}`
-      );
+      throw new Error(`Unexpected token: ${this.currentChar} at line ${this.line}, column ${this.column}`);
     }
 
     let eof = EOF_TOKEN;
     if (this.tokens.length > 0) {
-      const end = this.tokens[this.tokens.length - 1]
+      const end = this.tokens[this.tokens.length - 1];
       eof.loc = end.loc;
       eof.start = end.start;
       eof.end = end.end;
@@ -305,7 +284,7 @@ export default class Tokenizer {
 
   nextChar() {
     if (this.currentChar === null) {
-      return
+      return;
     }
 
     this.position++;
@@ -328,14 +307,14 @@ export default class Tokenizer {
     return {
       line: this.line,
       column: this.column,
-      index: this.position,
+      index: this.position
     };
   }
 
   createExtra(raw, value) {
     return {
       raw,
-      value,
+      value
     };
   }
 
@@ -347,11 +326,11 @@ export default class Tokenizer {
       value,
       loc: {
         start: startLoc,
-        end: endLoc,
+        end: endLoc
       },
       start: startLoc.index,
       end: endLoc.index,
-      extra,
+      extra
     };
   }
 
@@ -407,10 +386,7 @@ export default class Tokenizer {
     const loc = this.getLocation();
     let value = "";
 
-    while (
-      this.currentChar !== null &&
-      Tokenizer.ASCII_IDENTIFIER.has(this.currentChar)
-    ) {
+    while (this.currentChar !== null && Tokenizer.ASCII_IDENTIFIER.has(this.currentChar)) {
       value += this.currentChar;
       this.nextChar();
     }
@@ -433,9 +409,7 @@ export default class Tokenizer {
         break;
 
       default:
-        const type = Tokenizer.KEYWORD.has(value)
-          ? TOKEN_TYPES.Keyword
-          : TOKEN_TYPES.Identifier;
+        const type = Tokenizer.KEYWORD.has(value) ? TOKEN_TYPES.Keyword : TOKEN_TYPES.Identifier;
         this.addToken(type, value, loc);
         break;
     }
@@ -468,10 +442,7 @@ export default class Tokenizer {
     const loc = this.getLocation();
     let value = "";
 
-    while (
-      this.currentChar !== null &&
-      this.isNumeric(value + this.currentChar)
-    ) {
+    while (this.currentChar !== null && this.isNumeric(value + this.currentChar)) {
       value += this.currentChar;
       this.nextChar();
     }
@@ -481,23 +452,11 @@ export default class Tokenizer {
     }
 
     const { DECIMAL, BINARY, OCTAL, HEXADECIMAL } = Tokenizer.NUMERIC_REGEX;
-    if (
-      DECIMAL.test(value) ||
-      BINARY.test(value) ||
-      OCTAL.test(value) ||
-      HEXADECIMAL.test(value)
-    ) {
+    if (DECIMAL.test(value) || BINARY.test(value) || OCTAL.test(value) || HEXADECIMAL.test(value)) {
       const numeric = Number(value);
-      this.addToken(
-        TOKEN_TYPES.NumericLiteral,
-        numeric,
-        loc,
-        this.createExtra(value, numeric)
-      );
+      this.addToken(TOKEN_TYPES.NumericLiteral, numeric, loc, this.createExtra(value, numeric));
     } else {
-      throw new Error(
-        `Invalid numeric literal: ${value} at line ${loc.line}, column ${loc.column}`
-      );
+      throw new Error(`Invalid numeric literal: ${value} at line ${loc.line}, column ${loc.column}`);
     }
   }
 
@@ -598,9 +557,11 @@ export default class Tokenizer {
     while (this.currentChar !== null) {
       if (inTemplateExpression) {
         let braceDepth = 1;
+
         while (this.currentChar !== null && braceDepth > 0) {
           if (this.currentChar === "{") {
             braceDepth++;
+
             this.addToken(TOKEN_TYPES.Punctuator, "{", loc);
             this.nextChar();
             continue;
@@ -608,6 +569,7 @@ export default class Tokenizer {
 
           if (this.currentChar === "}") {
             braceDepth--;
+
             if (braceDepth === 0) {
               // close of the template expression
               this.addToken(TOKEN_TYPES.TemplateExpressionEnd, "}", loc);
@@ -625,10 +587,7 @@ export default class Tokenizer {
             continue;
           }
 
-          if (
-            this.currentChar === "/" &&
-            (this.peekChar() === "/" || this.peekChar() === "*")
-          ) {
+          if (this.currentChar === "/" && (this.peekChar() === "/" || this.peekChar() === "*")) {
             this.readComment();
             continue;
           }
@@ -653,17 +612,12 @@ export default class Tokenizer {
             continue;
           }
 
-          if (
-            Tokenizer.OPERATOR.has(this.currentChar) ||
-            Tokenizer.PUNCTUATOR.has(this.currentChar)
-          ) {
+          if (Tokenizer.OPERATOR.has(this.currentChar) || Tokenizer.PUNCTUATOR.has(this.currentChar)) {
             this.readOperatorOrPunctuator();
             continue;
           }
 
-          throw new Error(
-            `Unexpected token: ${this.currentChar} at line ${this.line}, column ${this.column}`
-          );
+          throw new Error(`Unexpected token: ${this.currentChar} at line ${this.line}, column ${this.column}`);
         }
 
         inTemplateExpression = false;
@@ -722,10 +676,7 @@ export default class Tokenizer {
       return Tokenizer.REGEX_START_KEYWORD.has(current.value);
     }
 
-    if (
-      Tokenizer.OPERATOR.has(current.value) ||
-      Tokenizer.PUNCTUATOR.has(current.value)
-    ) {
+    if (Tokenizer.OPERATOR.has(current.value) || Tokenizer.PUNCTUATOR.has(current.value)) {
       return Tokenizer.REGEX_START_OPERATOR.has(current.value);
     }
 
@@ -749,16 +700,9 @@ export default class Tokenizer {
     }
 
     if (value) {
-      this.addToken(
-        TOKEN_TYPES.RegularExpressionLiteral,
-        value,
-        loc,
-        this.createExtra(`/${value}/`, value)
-      );
+      this.addToken(TOKEN_TYPES.RegularExpressionLiteral, value, loc, this.createExtra(`/${value}/`, value));
     } else {
-      throw new Error(
-        `Unterminated regular expression literal at line ${loc.line}, column ${loc.column}`
-      );
+      throw new Error(`Unterminated regular expression literal at line ${loc.line}, column ${loc.column}`);
     }
   }
 
@@ -775,6 +719,7 @@ export default class Tokenizer {
     const wchar = twoChars + (this.peekChar(2) || "");
     if (Tokenizer.OPERATOR.has(wchar) || Tokenizer.PUNCTUATOR.has(wchar)) {
       value = wchar;
+
       if (this.peekChar() !== null) {
         this.nextChar();
 
@@ -786,28 +731,22 @@ export default class Tokenizer {
       this.nextChar();
     } else if (Tokenizer.OPERATOR.has(twoChars) || Tokenizer.PUNCTUATOR.has(twoChars)) {
       value = twoChars;
+
       if (this.peekChar() !== null) {
         this.nextChar();
       }
-  
+
       this.nextChar();
-    } else if (
-      Tokenizer.OPERATOR.has(this.currentChar) ||
-      Tokenizer.PUNCTUATOR.has(this.currentChar)
-    ) {
+    } else if (Tokenizer.OPERATOR.has(this.currentChar) || Tokenizer.PUNCTUATOR.has(this.currentChar)) {
       value = this.currentChar;
       this.nextChar();
     }
 
     if (value) {
-      const type = Tokenizer.OPERATOR.has(value)
-        ? TOKEN_TYPES.Operator
-        : TOKEN_TYPES.Punctuator;
+      const type = Tokenizer.OPERATOR.has(value) ? TOKEN_TYPES.Operator : TOKEN_TYPES.Punctuator;
       this.addToken(type, value, loc);
     } else {
-      throw new Error(
-        `Unexpected token: ${value} at line ${loc.line}, column ${loc.column}`
-      );
+      throw new Error(`Unexpected token: ${value} at line ${loc.line}, column ${loc.column}`);
     }
   }
 
@@ -845,9 +784,7 @@ export default class Tokenizer {
     if (value) {
       this.addToken(type, value, loc);
     } else {
-      throw new Error(
-        `Unterminated comment at line ${loc.line}, column ${loc.column}`
-      );
+      throw new Error(`Unterminated comment at line ${loc.line}, column ${loc.column}`);
     }
   }
-};
+}
