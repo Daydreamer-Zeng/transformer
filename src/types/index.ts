e47304cf2e9c54a4d3f03854e26c8105e7dd68e1
Daydreamer <971915386@qq.com>
2026-07-24 17:22:22 +0800

    feat(tokenizer): Implement tokenizer using TypeScript

---
+++ src/types/index.ts
@@ -0,0 +1,24 @@
+export interface TokenizerOptions {
+  plugins: string[]
+  strictMode: boolean
+  sourceType: "script" | "module"
+}
+
+export interface NormalizedConfig {
+  plugins: Set<string>
+  strictMode: boolean
+  sourceType: "script" | "module"
+}
+
+export interface Loc {
+  line: number
+  column: number
+  index: number
+}
+
+export interface Token {
+  type: string
+  start: Loc
+  end: Loc
+  [key: string]: any
+}
